6312ce7ced67765ff3fbda8fcb75098c1c4a55d0
Daniel Garza <danielg@Daniels-MacBook-Pro.local>
2021-01-16 17:18:11 -0600

    combined text and time components in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,42 @@
 import React from "react";
 import "./App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 
-import 'fontsource-roboto';
-import Container from '@material-ui/core/Container';
-
+import "fontsource-roboto";
+import Container from "@material-ui/core/Container";
 
 // import the ToDoList component
 import InputField from "./components/TextInput";
 import TimeDropdown from "./components/TimeDropdown";
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    "padding-top": "50px"
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  }
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <Container maxWidth="sm">
-        <InputField />
-        <TimeDropdown />
-      </Container>
+    <div className={classes.root}>
+      <Grid item xs={12}>
+        <Grid container justify="center" spacing={2}>
+          <Grid item>
+            <InputField className={classes.paper} />
+          </Grid>
+          <Grid item>
+            <TimeDropdown className={classes.paper} />
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 }
